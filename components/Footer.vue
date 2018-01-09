a88d5b3ca6bb2dc13b48ea210f8ebdc7c9237014
Woo vito <woo_vito@163.com>
2018-01-09 15:39:11 +0800

    Add files via upload
    Vue脚手架Demo

---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+	<footer>
+		<p> {{copyright}} </p>
+	</footer>
+</template>
+
+<script>
+export default {
+  name: 'app-footer',
+  data () {
+    return {
+    	copyright:"Copyright 2018 Vue Demo"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer{
+	background: #222;
+	padding: 6px;
+}
+p{
+	color: lightgreen;
+	text-align: center;
+}
+</style>
